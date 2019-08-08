e6a0c113fe06f2bddf6e988400f85655c4b1688c
Remy Mellet <remy.mellet@gmail.com>
2019-08-08 11:24:12 +0200

    test script

---
+++ test.php
@@ -0,0 +1,21 @@
+<?php
+
+
+$f = "/fine_lviv.json";
+echo "______ $f ______\n";
+$json = file_get_contents(__DIR__ . $f);
+echo "encoding: " . mb_detect_encoding($json) . "\n";
+echo "content: " . $json, "\n";
+$a = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
+print_r($a);
+
+$f = "/wrong_lviv.json";
+echo "______ $f ______\n";
+$json = file_get_contents(__DIR__ . $f);
+echo "encoding: " . mb_detect_encoding($json) . "\n";
+echo "content: " . $json, "\n";
+$a = json_decode($json, false, 512, JSON_THROW_ON_ERROR);
+print_r($a);
+//        $encoding = mb_check_encoding($json);
+//        $json = mb_convert_encoding($json, 'ascii');
+//$json = iconv("UTF-8", "ASCII//TRANSLIT", $json);
